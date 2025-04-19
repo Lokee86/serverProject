7ea5c22b6a2a6cde6a5a36b25c9bd43745ab1ec4
Lokee86 <otstructures@gmail.com>
2025-04-18 17:26:58 -0700

    added Handler

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // Create router and server
 func createServer() *http.Server {
 	router := http.NewServeMux()
+	router.Handle("/", rootFileHandler())
 
 	return &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
+}
+
+// create file handler
+func rootFileHandler() http.Handler {
+	return http.FileServer(http.Dir("."))
 }
 
 func main() {
