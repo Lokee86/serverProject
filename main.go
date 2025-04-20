32578a73bccb48777830f9a1e284cf65b2cf3a88
Lokee86 <otstructures@gmail.com>
2025-04-20 14:14:35 -0700

    added a metrics path

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// API hit counter increments
+func (apiCfg *apiConfig) incrementCoutner(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileserverHits.Add(1)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 // Create router and server
-func createServer() *http.Server {
+func createServer(apiCfg *apiConfig) *http.Server {
 	router := http.NewServeMux()
-	handler := http.FileServer(http.Dir("."))
+	handler := apiCfg.incrementCoutner(http.FileServer(http.Dir(".")))
 	router.Handle("/app/", http.StripPrefix("/app/", handler))
 	router.HandleFunc("/healthz", func(response http.ResponseWriter, r *http.Request) {
+		response.WriteHeader(http.StatusOK)
+		response.Write([]byte("OK"))
+	})
+	router.HandleFunc("/metrics", func(response http.ResponseWriter, r *http.Request) {
 		response.WriteHeader(http.StatusOK)
 		response.Write([]byte("OK"))
 	})
@@ -21,6 +38,7 @@ func createServer() *http.Server {
 }
 
 func main() {
-	server := createServer()
+	apiCfg := &apiConfig{}
+	server := createServer(apiCfg)
 	server.ListenAndServe()
 }
