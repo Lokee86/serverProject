722edbc86c5f07fad1b7c26e1ec02244600ccec5
Lokee86 <otstructures@gmail.com>
2025-04-20 15:02:30 -0700

    added apiConfig struct and methods. /metrics and /reset paths active

--- main.go
+++ main.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"sync/atomic"
 )
 
+const pathRoot = "."
+const port = ":8080"
+
 type apiConfig struct {
-	fileserverHits atomic.Int32
+	fileServerHits atomic.Int32
 }
 
-// API hit counter increments
-func (apiCfg *apiConfig) incrementCoutner(handler http.Handler) http.Handler {
+// increment hit counter
+func (a *apiConfig) serverHitCounter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		apiCfg.fileserverHits.Add(1)
-		handler.ServeHTTP(w, r)
+		a.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
 	})
+}
+
+// metrics handler
+func (a *apiConfig) metricsHandler(response http.ResponseWriter, r *http.Request) {
+	log.Printf("Hits: %v", a.fileServerHits.Load())
+	response.WriteHeader(http.StatusOK)
+	response.Write([]byte("metrics"))
+}
+
+// reset counter
+func (a *apiConfig) resetCounter(response http.ResponseWriter, r *http.Request) {
+	a.fileServerHits.Store(0)
+	response.WriteHeader(http.StatusOK)
+	response.Write([]byte("Hits coutner reset to 0"))
+	log.Println("Hit counter reset to 0")
+
 }
 
 // Create router and server
 func createServer(apiCfg *apiConfig) *http.Server {
 	router := http.NewServeMux()
-	handler := apiCfg.incrementCoutner(http.FileServer(http.Dir(".")))
-	router.Handle("/app/", http.StripPrefix("/app/", handler))
+	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(pathRoot)))
+	router.Handle("/app/", apiCfg.serverHitCounter(handler))
+	router.HandleFunc("/metrics", apiCfg.metricsHandler)
+	router.HandleFunc("/reset", apiCfg.resetCounter)
 	router.HandleFunc("/healthz", func(response http.ResponseWriter, r *http.Request) {
-		response.WriteHeader(http.StatusOK)
-		response.Write([]byte("OK"))
-	})
-	router.HandleFunc("/metrics", func(response http.ResponseWriter, r *http.Request) {
 		response.WriteHeader(http.StatusOK)
 		response.Write([]byte("OK"))
 	})
 
 	return &http.Server{
-		Addr:    ":8080",
+		Addr:    port,
 		Handler: router,
 	}
 }
@@ -40,5 +58,6 @@ func createServer(apiCfg *apiConfig) *http.Server {
 func main() {
 	apiCfg := &apiConfig{}
 	server := createServer(apiCfg)
-	server.ListenAndServe()
+	log.Printf("Server running on Port%v from %v", port, pathRoot)
+	log.Fatal(server.ListenAndServe())
 }
